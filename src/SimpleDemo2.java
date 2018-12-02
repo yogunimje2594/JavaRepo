856e745ea943a0445822fcbbd53cb135320bda97
yogeshnimje <39332931+yogeshnimje@users.noreply.github.com>
2018-12-02 15:51:50 +0530

    SimpleDemo2 modification added

--- src/SimpleDemo2.java
+++ src/SimpleDemo2.java
@@ -4,6 +4,7 @@ public class SimpleDemo2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Git");
+		System.out.println("SimpleDemo2 file modification done");
 	}
 
 }
